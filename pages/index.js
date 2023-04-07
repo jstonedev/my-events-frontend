6fee04f6b83612d364e21a5e8d9acbcd2e0eab32
jstone <jstonedev521@gmail.com>
2023-04-07 15:05:40 -0400

    Module aliases

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import Layout from '../components/Layout'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
+import Layout from 'components/Layout'
 
 export default function HomePage() {
   return (
