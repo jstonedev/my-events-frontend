9957643b01a36e4aa82bcfb864dd5a53d84e4150
jstone <jstonedev521@gmail.com>
2023-04-12 11:30:47 -0400

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
+import { API_URL } from 'config/index'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
 import Layout from 'components/Layout'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Home</h1>
+      {events.length === 0 && 
+        <h3>No upcoming events</h3>
+}
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {events}
+  }
 }
