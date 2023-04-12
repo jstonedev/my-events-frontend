64991d3b24fb6d6b9c80f55178fe57ac2a973107
jstone <jstonedev521@gmail.com>
2023-04-12 14:28:19 -0400

    Single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,50 @@
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
 import { API_URL } from "@/config/index";
 import Layout from "components/Layout";
+import styles from "styles/Event.module.css";
+import Image from "next/image";
 
 export default function EventPage({ evt }) {
+	const deleteEvent = (e) => {
+		console.log("delete");
+	};
+
 	return (
 		<Layout>
-			<h1>{evt.name}</h1>
+			<div className={styles.event}>
+				<div className={styles.controls}>
+					<Link href={`/events/edit/${evt.id}`}>
+						<FaPencilAlt /> Edit Event
+					</Link>
+					<a href='#' className={styles.delete} onClick={deleteEvent}>
+						<FaTimes /> Delete Event
+					</a>
+				</div>
+
+				<span>
+					{evt.date} at {evt.time}
+				</span>
+				<h1>{evt.name}</h1>
+				{evt.image && (
+					<div className={styles.image}>
+						<Image src={evt.image} width={960} height={600} alt='' />
+					</div>
+				)}
+
+				<h3>Performers:</h3>
+				<p>{evt.performers}</p>
+				<h3>Description:</h3>
+				<p>{evt.description}</p>
+				<h3>Venue: {evt.venue}</h3>
+				<p>{evt.address}</p>
+
+				{/* TODO: MAP */}
+
+				<Link href='/events' className={styles.back}>
+					{"<"} Go Back
+				</Link>
+			</div>
 		</Layout>
 	);
 }
