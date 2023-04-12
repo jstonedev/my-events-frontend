6cc72c5d4b9a7e74edde52fe06cf548fe2331abb
jstone <jstonedev521@gmail.com>
2023-04-12 12:16:38 -0400

    Display events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,26 @@
+import { API_URL } from "config/index";
 import Layout from "components/Layout";
+import EventItem from "components/EventItem";
 
-export default function Events() {
-  return (
-    <Layout>
-      <h1>Upcoming Events</h1>
-    </Layout>
-  )
+export default function EventsPage({ events }) {
+	return (
+		<Layout>
+			<h1>Events</h1>
+			{events.length === 0 && <h3>No upcoming events</h3>}
+
+			{events.map((evt) => (
+				<EventItem key={evt.id} evt={evt} />
+			))}
+		</Layout>
+	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+
+	return {
+		props: { events },
+		revalidate: 1,
+	};
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
-import { API_URL } from 'config/index'
-import Footer from 'components/Footer'
-import Header from 'components/Header'
-import Layout from 'components/Layout'
+import { API_URL } from "config/index";
+import Link from "next/link";
+import Layout from "components/Layout";
+import EventItem from "components/EventItem";
 
 export default function HomePage({ events }) {
-  return (
-    <Layout>
-      <h1>Home</h1>
-      {events.length === 0 && 
-        <h3>No upcoming events</h3>
-}
-    </Layout>
-  )
+	return (
+		<Layout>
+			<h1>Upcoming Events</h1>
+			{events.length === 0 && <h3>No upcoming events</h3>}
+
+			{events.map((evt) => (
+				<EventItem key={evt.id} evt={evt} />
+			))}
+
+			{events.length > 0 && (
+				<Link href='/events' className='btn-secondary'>
+					View All Events
+				</Link>
+			)}
+		</Layout>
+	);
 }
 
-export async function getServerSideProps() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
+export async function getStaticProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
 
-  return {
-    props: {events}
-  }
+	return {
+		props: { events: events.slice(0, 3) },
+		revalidate: 1,
+	};
 }
